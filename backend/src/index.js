1b69fd044fee3225f36b9600f1a51e327944ec71
gdevansh16082004 <gdevansh338@gmail.com>
2025-01-10 23:57:52 +0530

    basic features done

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from "cors"
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+import cookieparser from 'cookie-parser'
+import messageRoutes from "./routes/message.route.js"
+import { app, server } from './lib/socket.js';
+import path from "path"
+dotenv.config();
+
+
+app.use(express.json({ limit: "10mb" })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}
+));
+app.use(express.json());
+app.use(cookieparser());
+
+
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req,res)=> {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+app.use()
+
+server.listen(port,()=>{
+    console.log('server is running on port', port);
+    connectDB();
+})
