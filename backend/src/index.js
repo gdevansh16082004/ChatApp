e2695f4b7b460211095d21f8dcb436e440231397
Devansh Gupta <148975720+gdevansh16082004@users.noreply.github.com>
2025-01-11 19:59:22 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ import { connectDB } from './lib/db.js';
 import cookieparser from 'cookie-parser'
 import messageRoutes from "./routes/message.route.js"
 import { app, server } from './lib/socket.js';
-import path from "path"
+// import path from "path"
 dotenv.config();
 
 
@@ -22,18 +22,18 @@ app.use(cookieparser());
 
 
 const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*", (req,res)=> {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
-}
+//     app.get("*", (req,res)=> {
+//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//     })
+// }
 
 server.listen(port,()=>{
     console.log('server is running on port', port);
