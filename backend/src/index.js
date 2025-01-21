59a1cfa702e421d5ab21d883658e0f4cf2d16594
gdevansh16082004 <gdevansh338@gmail.com>
2025-01-22 00:17:50 +0530

    updated index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,10 @@ import cookieparser from 'cookie-parser'
 import messageRoutes from "./routes/message.route.js"
 import { app, server } from './lib/socket.js';
 import path from "path"
-dotenv.config();
 
+dotenv.config();
+const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // Increase JSON payload limit
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -20,10 +22,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieparser());
 
-
-const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
-
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
@@ -32,9 +30,8 @@ if(process.env.NODE_ENV === "production"){
 
     app.get("*", (req,res)=> {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+    });
 }
-app.use()
 
 server.listen(port,()=>{
     console.log('server is running on port', port);
