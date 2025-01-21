5e7d20c096a884095ac0fbf723e241da5d33c4e9
Devansh Gupta <148975720+gdevansh16082004@users.noreply.github.com>
2025-01-22 00:27:50 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,34 +6,32 @@ import { connectDB } from './lib/db.js';
 import cookieparser from 'cookie-parser'
 import messageRoutes from "./routes/message.route.js"
 import { app, server } from './lib/socket.js';
-// import path from "path"
-dotenv.config();
+import path from "path"
 
+dotenv.config();
+const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // Increase JSON payload limit
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin: "https://chatapp-frontend-zgs6.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }
 ));
 app.use(express.json());
 app.use(cookieparser());
 
-
-const port = process.env.PORT || 5000;
-// const __dirname = path.resolve();
-
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-//     app.get("*", (req,res)=> {
-//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//     })
-// }
+    app.get("*", (req,res)=> {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(port,()=>{
     console.log('server is running on port', port);
