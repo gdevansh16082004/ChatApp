4b4e1b392c897796bc28f8a203d58c8c1a24987f
gdevansh16082004 <gdevansh338@gmail.com>
2025-12-30 00:54:51 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" })); // Increase JSON payload limit
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatapp-1-gmrb.onrender.com",
     credentials: true
 }
 ));
